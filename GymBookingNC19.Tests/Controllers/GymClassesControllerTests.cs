ea2c440fd8efebfc6d389260897ccfa3eed9572c
DZM <d.bjorlingh@gmail.com>
2020-01-20 14:10:11 +0100

    Add First Test

--- GymBookingNC19.Tests/Controllers/GymClassesControllerTests.cs
+++ GymBookingNC19.Tests/Controllers/GymClassesControllerTests.cs
@@ -1,9 +1,13 @@
 using GymBookingNC19.Core;
 using GymBookingNC19.Core.Models;
+using GymBookingNC19.Core.ViewModels;
 using GymBookingNC19.Data.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System.Threading.Tasks;
 
 namespace GymBookingNC19.Controllers
 {
@@ -11,10 +15,13 @@ namespace GymBookingNC19.Controllers
     public class GymClassesControllerTests
     {
 
+        private Mock<IGymClassesRepository> repository;
+        private GymClassesController controller;
+
         [TestInitialize]
         public void SetUp()
         {
-            var repository = new Mock<IGymClassesRepository>();
+            repository = new Mock<IGymClassesRepository>();
             var mockUoW = new Mock<IUnitOfWork>();
             mockUoW.Setup(u => u.GymClasses).Returns(repository.Object);
 
@@ -22,13 +29,31 @@ namespace GymBookingNC19.Controllers
             var mockUsermanager = new Mock<UserManager<ApplicationUser>>
                 (userStore.Object, null, null, null, null, null, null, null, null);
 
-            var controller = new GymClassesController(mockUsermanager.Object, mockUoW.Object);
+           controller = new GymClassesController(mockUsermanager.Object, mockUoW.Object);
 
         }
 
-        [TestMethod]
-        public void TestMethod1()
+        private void SetUpUserIsAuthenticated(Controller controller, bool isAuthenticated)
         {
+            var mockContext = new Mock<HttpContext>(MockBehavior.Default);
+            mockContext.SetupGet(httpCon => httpCon.User.Identity.IsAuthenticated).Returns(isAuthenticated);
+            controller.ControllerContext = new ControllerContext { HttpContext = mockContext.Object };
+        }
+
+
+
+        [TestMethod]
+        public async Task Index_ReturnsViewResult_ShouldPass()
+        {
+            //Arrange
+            SetUpUserIsAuthenticated(controller, true);
+            var vm = new IndexViewModel { History = false };
+
+            //Act
+            var actual = await controller.Index(vm);
+
+            //Assert
+            Assert.IsInstanceOfType(actual, typeof(ViewResult));
         }
     }
 }
